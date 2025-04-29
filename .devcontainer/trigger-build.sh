7b18cd89598c66c7afa3de382078f2f7e39d904a
Michele Sciabarra <michele@sciabarra.com>
2025-04-29 16:38:02 +0100

    trigger build for 2504291600

--- .devcontainer/trigger-build.sh
+++ .devcontainer/trigger-build.sh
@@ -1,4 +1,6 @@
 TAG=$(grep 'mastrogpt/agent41-starter' docker-compose.yml | awk -F: '{print $3}')
+git tag -d $TAG
+git push origin --delete $TAG
 git commit -m "trigger build for $TAG" -a
 git tag $TAG
 git push origin main --tags
